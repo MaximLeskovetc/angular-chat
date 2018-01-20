2108441c15e213c6cbe983d7d8c4345dbf7431fa
LeskovetcMaxim <leskovstudio@gmail.com>
2018-01-20 15:50:32 +0300

    Добавление дизайна

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
 import {Component} from '@angular/core';
 import * as io from 'socket.io-client';
 
+class Message {
+    content: string;
+    author: string;
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
     socket;
-    messages: object[] = [];
+    messagesList: object[] = [];
+    message: Message = new Message();
 
     constructor() {
         this.socket = io();
         this.socket.on('chat message', (msg) => {
-            this.messages.push(msg);
+            this.messagesList.push(msg);
         });
+        this.message.author = 'User Name';
     }
 
     send() {
-        this.socket.emit('chat message', 'Hello world');
+        this.socket.emit('chat message', JSON.stringify(this.message));
     }
 }
