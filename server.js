85ba258caab45617ca8c265141724f43af60de0b
LeskovetcMaxim <leskovstudio@gmail.com>
2018-01-20 14:19:27 +0300

    Adding socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('User connection');
+    io.on('disconnect', () => {
+        console.log('User disconnect');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(port, () => {
+    console.log('Server running port ' + port);
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    socket;
+    messages: object[] = [];
+
+    constructor() {
+        this.socket = io();
+        this.socket.on('chat message', (msg) => {
+            this.messages.push(msg);
+        });
+    }
+
+    send() {
+        this.socket.emit('chat message', 'Hello world');
+    }
 }
